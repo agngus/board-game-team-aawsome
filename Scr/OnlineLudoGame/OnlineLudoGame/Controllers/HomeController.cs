1dd8e250b8a62d9b3fb0c3bb871fe69cae6a9106
SonnyAasen <35238145+SonnyAasen@users.noreply.github.com>
2018-01-17 13:34:02 +0100

    Added ActionResult LudoBoard in Controller Added view LudoBoard

--- Scr/OnlineLudoGame/OnlineLudoGame/Controllers/HomeController.cs
+++ Scr/OnlineLudoGame/OnlineLudoGame/Controllers/HomeController.cs
@@ -13,5 +13,11 @@ namespace OnlineLudoGame.Controllers
         {
             return View();
         }
+
+        public ActionResult LudoBoard()
+        {
+            return View();
+        }
+        
     }
 }
