da079e6bab5ad5598f3db946444e06c116b52d2e
SonnyAasen <35238145+SonnyAasen@users.noreply.github.com>
2018-01-19 21:15:05 +0100

    Added Dice class in game engine. Added Dice roll method.

---
+++ Scr/OnlineLudoGame/Gameengine/Dice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gameengine
+{
+    class Dice
+    {
+        public int RollDice()
+        {
+            int result = 0;
+            Random random = new Random();
+            result = random.Next(1, 6);
+            return result;
+        }   
+    }
+}
