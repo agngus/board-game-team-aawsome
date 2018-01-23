aaa77de7c9328fdc8db1b1b7a6c46a1127f25c07
Agneta Gustafsson <agneta.kode@outlook.com>
2018-01-23 13:36:42 +0100

    spelbricka

---
+++ Scr/OnlineLudoGame/Gameengine/angelic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gameengine
+{
+    public class angelic
+    {
+        public static string text()
+        {
+            string x = "x";
+            return x;
+        }
+    }
+}
