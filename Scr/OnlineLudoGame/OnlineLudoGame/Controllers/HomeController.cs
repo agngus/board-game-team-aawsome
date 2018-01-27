7b5cd05e9acde60724d9edaf4c9bf51d38fb2ca8
mmmolin78 <mmmolin78@gmail.com>
2018-01-28 00:20:32 +0100

    Moved code to Methods in GameEngine, Implemented some OOP, lots of refactoring.

--- Scr/OnlineLudoGame/OnlineLudoGame/Controllers/HomeController.cs
+++ Scr/OnlineLudoGame/OnlineLudoGame/Controllers/HomeController.cs
@@ -12,8 +12,7 @@ namespace OnlineLudoGame.Controllers
     public class HomeController : Controller
     {
         public string PlayerID { get; private set; }
-        public static int GameID { get; set; }
-        private static bool IsStart = true;
+        public static int GameID { get; set; }        
 
         // GET: Home        
         public ActionResult StartPage()
@@ -52,14 +51,13 @@ namespace OnlineLudoGame.Controllers
             // Find some way to fetch GameID
             string cookieValue = Request.Cookies["User"].Value;
             var gameSession = Gameengine.ActiveGame.GetSession(cookieValue);
-
             string[] side = new string[9];
             for (int i = 0; i < 9; i++)
             {
                 try
                 {
                     //if (Gameengine.ActiveGame.Game[0].Board[i].Side != null)
-                    if(gameSession.Board[i].Side != null)
+                    if (gameSession.Board[i].Side != null)
                     {
                         side[i] = gameSession.Board[i].Side;
                     }
@@ -70,7 +68,7 @@ namespace OnlineLudoGame.Controllers
                 }
             }
             var board = new Board
-            {  
+            {
                 GameID = GameID,
                 Cell1 = side[0],
                 Cell2 = side[1],
@@ -85,35 +83,15 @@ namespace OnlineLudoGame.Controllers
             return View(board);
         }
 
-        public ActionResult PlayerMove(string buttonclickid)
+        public ActionResult PlayerMove(string buttonClick)
         {
-            string currentPlayer = Request.Cookies["User"].Value;
+            string cookieValue = Request.Cookies["User"].Value;
+            var gameSession = Gameengine.ActiveGame.GetSession(cookieValue);
             try
             {
-                if (IsStart == true)
-                {
-                    var session = Gameengine.ActiveGame.Game.FindIndex(x => x.Players[0].PlayerID == currentPlayer);
-                    if (currentPlayer == Gameengine.ActiveGame.Game[session].Players[0].PlayerID)
-                    {
-                        var player1 = Gameengine.ActiveGame.Game[session].Players[0];
-                        int indexPressed = int.Parse(buttonclickid) - 1;
-                        Gameengine.ActiveGame.Game[session].Board[indexPressed] = player1;
-                        IsStart = false;
-                    }
-                }
-                else if (IsStart == false)
-                {
-                    var session = Gameengine.ActiveGame.Game.FindIndex(x => x.Players[1].PlayerID == currentPlayer);
-                    if (currentPlayer == Gameengine.ActiveGame.Game[session].Players[1].PlayerID)
-                    {
-                        var player2 = Gameengine.ActiveGame.Game[session].Players[1];
-                        int indexPressed = int.Parse(buttonclickid) - 1;
-                        Gameengine.ActiveGame.Game[session].Board[indexPressed] = player2;
-                        IsStart = true;
-                    }
-                }
+                gameSession.Turn(cookieValue, buttonClick);
             }
-            catch { }
+            catch { }           
             return RedirectToAction("Game");
         }
     }
