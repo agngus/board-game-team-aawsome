eb28835c737e6b8bd03dc1aba8278b1e5dd6ef4e
mmmolin78 <mmmolin78@gmail.com>
2018-01-28 20:34:07 +0100

    routing issues rollback

--- Scr/OnlineLudoGame/OnlineLudoGame/App_Start/RouteConfig.cs
+++ Scr/OnlineLudoGame/OnlineLudoGame/App_Start/RouteConfig.cs
@@ -18,7 +18,7 @@ namespace OnlineLudoGame
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "TableOfContent", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "StartPage", id = UrlParameter.Optional }
             );
         }
     }
