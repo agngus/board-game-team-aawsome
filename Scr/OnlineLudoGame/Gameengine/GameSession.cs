b571e643fd5d3a6c84dd0af1bc55276f61c2cca0
Agneta Gustafsson <agneta.kode@outlook.com>
2018-01-29 23:23:46 +0100

    inga medvetna ändringar.

--- Scr/OnlineLudoGame/Gameengine/GameSession.cs
+++ Scr/OnlineLudoGame/Gameengine/GameSession.cs
@@ -87,35 +87,35 @@ namespace Gameengine
             string test = "";
             if (this.Board[0].Side.Equals(this.Board[1].Side) && this.Board[0].Side.Equals(this.Board[2].Side))
             {
-                test = this.Board[0].Name + " WON THE GAME!";                
+                test = this.Board[0].Name + " WON THE GAME!";
             }
             else if (this.Board[3].Side.Equals(this.Board[4].Side) && this.Board[3].Side.Equals(this.Board[5].Side))
             {
-                test = this.Board[3].Name + " WON THE GAME!";                
+                test = this.Board[3].Name + " WON THE GAME!";
             }
             else if (this.Board[6].Side.Equals(this.Board[7].Side) && this.Board[6].Side.Equals(this.Board[8].Side))
             {
-                test = this.Board[6].Name + " WON THE GAME!";                
+                test = this.Board[6].Name + " WON THE GAME!";
             }
             else if (this.Board[0].Side.Equals(this.Board[3].Side) && this.Board[0].Side.Equals(this.Board[6].Side))
             {
-                test = this.Board[0].Name + " WON THE GAME!";                
+                test = this.Board[0].Name + " WON THE GAME!";
             }
             else if (this.Board[1].Side.Equals(this.Board[4].Side) && this.Board[1].Side.Equals(this.Board[7].Side))
             {
-                test = this.Board[1].Name + " WON THE GAME!";                
+                test = this.Board[1].Name + " WON THE GAME!";
             }
             else if (this.Board[2].Side.Equals(this.Board[5].Side) && this.Board[2].Side.Equals(this.Board[8].Side))
             {
-                test = this.Board[2].Name + " WON THE GAME!";                
+                test = this.Board[2].Name + " WON THE GAME!";
             }
             else if (this.Board[0].Side.Equals(this.Board[4].Side) && this.Board[0].Side.Equals(this.Board[8].Side))
             {
-                test = this.Board[0].Name + " WON THE GAME!";                
+                test = this.Board[0].Name + " WON THE GAME!";
             }
             else if (this.Board[2].Side.Equals(this.Board[4].Side) && this.Board[2].Side.Equals(this.Board[6].Side))
             {
-                test = this.Board[2].Name + " WON THE GAME!";                
+                test = this.Board[2].Name + " WON THE GAME!";
             }
             else if (!this.Board[0].Side.Equals("-") && !this.Board[1].Side.Equals("-") && !this.Board[2].Side.Equals("-") &&
                 !this.Board[3].Side.Equals("-") && !this.Board[4].Side.Equals("-") && !this.Board[5].Side.Equals("-") &&
@@ -142,6 +142,8 @@ namespace Gameengine
             int result = random.Next(10000, 50000);
             return result;
         }
+       
     }
+
 
 }
