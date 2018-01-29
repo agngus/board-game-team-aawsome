f48b9aaf9f12be342a509c38ebf59bec8f49a37a
mmmolin78 <mmmolin78@gmail.com>
2018-01-29 20:51:02 +0100

    login input now required

--- Scr/OnlineLudoGame/OnlineLudoGame/Models/Player.cs
+++ Scr/OnlineLudoGame/OnlineLudoGame/Models/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,9 @@ namespace OnlineLudoGame.Models
 {
     public class Player
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
     }
 }
